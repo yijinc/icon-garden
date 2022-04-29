a1bb81d7c9a26c4e6920469cbd00cc9f339a552c
yijinc <yijinc@qq.com>
2022-04-29 11:53:00 +0800

    feat: declare JSX for vue

--- packages/vue/src/type.d.ts
+++ packages/vue/src/type.d.ts
@@ -1,3 +1,15 @@
+import Vue, { VNode } from "vue";
+
+declare global {
+  namespace JSX {
+    interface Element extends VNode {}
+    interface ElementClass extends Vue {}
+    interface IntrinsicElements {
+      [elem: string]: any;
+    }
+  }
+}
+
 export type IIconProps = {
   // 图标大小
   size: number | string;
