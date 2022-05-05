0e617551ccfd2d8eb100b4d8c797bd0adda0a229
yijinc <yijinc@qq.com>
2022-05-05 16:24:52 +0800

    feat: add icon demo

---
+++ src/components/IconList/index.tsx
@@ -0,0 +1,40 @@
+/**
+ * 图标列表
+ * **/
+import React from 'react';
+import styles from './styles.module.css';
+import { DownloadBaseOutline } from '@icon-garden/react';
+
+export type IconItem = {
+  name: string;
+  icon: typeof DownloadBaseOutline;
+  size?: number;
+  color?: string;
+};
+
+type IProps = {
+  icons: IconItem[];
+}
+
+export default ({ icons }: IProps) => {
+
+  const clickItem = (item) => {
+  };
+
+  return (
+    <div className={styles.container}>
+      {
+        icons.map(item => {
+          const { name, icon: Icon } = item;
+          return <div key={name} className={styles.item} onClick={() => clickItem(item)}>
+            <div className={styles.name}>{name}</div>
+            <Icon size="32" fill="#000000" />
+            <div className={styles.footer}>
+              <DownloadBaseOutline fill="#b0b1b8" size="14" />
+            </div>
+          </div>
+        })
+      }
+    </div>
+  )
+}
