74baa796356851298c03af8e66ed4a2bc172a4d7
yijinc <yijinc@qq.com>
2022-05-05 10:20:11 +0800

    fix： vue3 setup return render function

--- build/utils/template-vue3.ts
+++ build/utils/template-vue3.ts
@@ -32,7 +32,7 @@ export default (name: string, content: string): string => {
      }
    },
    setup: (props: IIconProps) => {
-     return (
+     return () => (
        ${xml.toString()}
      );
    }
